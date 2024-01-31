0c318950f7f352577498f6f7ca04ed7e89e8a0db
Portolio <Denisng666@gmail.com>
2024-01-31 20:54:45 +0100

    save for today

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -10,62 +10,26 @@ const handler: NextApiHandler = (req, res) => {
 
     switch(method) {
         case 'GET': 
-            return res.json({ Ok : true })
+        const data = readPostsInfo()
+            return res.json({ postInfo : data })
         default:
             return res.status(404).send('Not Found 123' )
 
-
     }
+}
 
-// console.log(req.method)
-
-// const readPostsInfo = () => {
-//     const readPath = path.join(process.cwd(), 'posts');
-//     const dirs = fs.readdirSync(readPath);
-//     dirs.map((filename) => {
-//         const PathreadPath = path.join(process.cwd(), 'posts/ ' + filename );
-//         const filecontent = fs.readFileSync(PathreadPath, {encoding: 'utf-8'})
-//         console.log(matter(filecontent)) 
-
-//     })
-//     return('')
-// }
-
+const readPostsInfo = () => {
+    const dirPathToRead = path.join(process.cwd(), "posts")
+   const dirs = fs.readdirSync(dirPathToRead);
+  const data = dirs.map((filename) => {
+    const filePathToRead = path.join(process.cwd(), 'posts/' + filename);
+    const fileContenct = fs.readFileSync(filePathToRead, {encoding: 'utf-8'})
+  return matter(fileContenct).data
+   })
+   return data
 }
 
  
 export default handler;
 
 
-// import { NextApiHandler } from "next";
-// import fs from 'fs';
-// import path from "path";
-
-// const handler: NextApiHandler = (req, res) => {
-//     const { method } = req;
-
-//     switch (method) {
-//         case 'GET': {
-//             const data = readPostsInfo();
-//             return res.json({ postInfo: data });
-//         }
-//         default:
-//             res.status(404).send('Not Found 123');
-//     }
-// };
-
-// const readPostsInfo = () => {
-//     const readPath = path.join(process.cwd(), 'posts');
-//     const dirs = fs.readdirSync(readPath);
-
-//     const contentArray = dirs.map((filename) => {
-//         const filePath = path.join(process.cwd(), 'posts', filename);
-//         const fileContent = fs.readFileSync(filePath, 'utf-8'); // Specify encoding
-//         console.log(fileContent);
-//         return { filename, content: fileContent }; // Return an object with filename and content
-//     });
-
-//     return contentArray;
-// };
-
-// export default handler;
--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { NextPage } from "next";
 import BlogCard from "./component/blogCard";
 
@@ -6,19 +7,22 @@ interface Props {
 }
 
 const Blogs: NextPage<Props> = () => {
+
+const [ posts, setPosts ] = useState<{ title: string, slug: string, meta: string }[]>([]);
+
+const fetchPosts = async () => {
+    const res = await fetch('/api/posts').then(data => data.json());
+    setPosts(res.postInfo) 
+}
+
+ useEffect(() => {
+fetchPosts() 
+}, [])
+
     return(
         <>
         <div className=" bg-blue-200 p-5 max-w-3xl mx-auto rounded">
-            <h1 className="text-3xl font-semibold bg-gray-200"> Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
-            <p className="text-purple-900">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed ipsa unde maxime esse fugit est reiciendis neque hic.</p>
-        </div>
-        <div className=" max-w-3xl mx-auto p-5  space-y-5 font-semibold bg-green-900">
-            <BlogCard title="This is the Title !
-            " description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe fugit nisi alias assumenda molestias! Vero aliquam rerum optio aperiam voluptas!
-            " />
-            <BlogCard title="This is the title" 
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe fugit nisi alias assumenda molestias! Vero aliquam rerum optio aperiam voluptas!
-            " />
+         {posts.map(post => <BlogCard title={post.title} description={post.meta}/>)}
         </div>
         </>
     )
