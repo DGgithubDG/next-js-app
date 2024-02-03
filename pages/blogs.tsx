135fbbae280adada0a32ffff1296ae26c12c49ac
Portolio <Denisng666@gmail.com>
2024-02-03 19:37:03 +0100

    save

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,28 +1,47 @@
 import { useState, useEffect } from "react";
-import { NextPage } from "next";
-import BlogCard from "./component/blogCard";
+import { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
+import BlogCard from "./components/blogCard";
+import { Infer } from "next/dist/compiled/superstruct";
 
-interface Props {
-
+interface PostAPIResponse {
+    postInfo: {
+        title: string ;
+        slug: string;
+        meta: string
+    } []
 }
 
-const Blogs: NextPage<Props> = () => {
 
-const [ posts, setPosts ] = useState<{ title: string, slug: string, meta: string }[]>([]);
+export const getStaticProps = async () => {
+    const {postInfo}: PostAPIResponse = await fetch('http://localhost:3000/api/posts').then(data => data.json());
 
-const fetchPosts = async () => {
-    const res = await fetch('/api/posts').then(data => data.json());
-    setPosts(res.postInfo) 
+    return {
+        props: {posts: postInfo}
+    }
 }
 
- useEffect(() => {
-fetchPosts() 
-}, [])
+
+
+// const [ posts, setPosts ] = useState<{ title: string, slug: string, meta: string }[]>([]);
+
+// const fetchPosts = async () => {
+//     const res = await fetch('/api/posts').then(data => data.json());
+//     setPosts(res.postInfo) 
+// }
+
+//  useEffect(() => {
+// fetchPosts() 
+// }, [])
+
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>
+
+const Blogs: NextPage<Props> = ({ posts }: InferGetStaticPropsType <typeof getStaticProps>) => {
 
     return(
         <>
         <div className=" bg-blue-200 p-5 max-w-3xl mx-auto rounded">
-         {posts.map(post => <BlogCard title={post.title} description={post.meta}/>)}
+         {posts.map((post) => <BlogCard key={post.slug} title={post.title} description={post.meta} slug={post.slug}/>)}
         </div>
         </>
     )
---
+++ pages/components/blogCard.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import Link from 'next/link';
+
+interface Props {
+  title: string;
+  description: string;
+  slug: string;
+
+}
+
+const BlogCard: FC<Props> = ({ title, description, slug }): JSX.Element => {
+  return (
+      <Link href={'/blogs/' + slug}>
+        <div className='block'>
+        <h1 className='text-3xl '>{title}</h1>
+      <p>{description}</p>
+      </div>
+      </Link>
+ 
+      
+  );
+};
+
+export default BlogCard;
+2
